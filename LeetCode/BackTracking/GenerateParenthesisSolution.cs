07d2d3060362957a41856dd9e4c78cd365b4d450
Martin Indzhov | mind <martin.indzhov@zuhlke.com>
2021-04-19 13:56:08 +0300

    Generate Parenthesis solution

---
+++ LeetCode/BackTracking/GenerateParenthesisSolution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.BackTracking
+{
+    public class GenerateParenthesisSolution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+            FillArr(result, new StringBuilder(), 0, 0, n * 2);
+            return result;
+        }
+
+        public void FillArr(IList<string> list, StringBuilder currentWord, int open, int close, int max)
+        {
+            if (currentWord.Length == max)
+            {
+                list.Add(currentWord.ToString());
+                return;
+            }
+            if (open * 2 < max)
+            {
+                currentWord.Append("(");
+                FillArr(list, currentWord, open + 1, close, max);
+                currentWord.Remove(currentWord.Length - 1, 1);
+            }
+
+            if (close < open)
+            {
+                currentWord.Append(")");
+                FillArr(list, currentWord, open, close + 1, max);
+                currentWord.Remove(currentWord.Length - 1, 1);
+            }
+        }
+    }
+}
